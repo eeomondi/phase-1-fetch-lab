a9c86e3ffb50898a17a1bd4b6cca8ed8d89aaffa
Elvis <elvis.omondi@student.moringaschool.com>
2024-07-23 20:20:28 +0300

    commit

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 async function fetchBooks() {
   try {
     const response = await window.fetch('https://anapioficeandfire.com/api/books');
@@ -10,9 +9,9 @@ async function fetchBooks() {
 }
 
 function renderBooks(books) {
-  const mainElement = document.querySelector('main');
+  const mainElement = window.document.querySelector('main');
   books.forEach((book) => {
-    const bookTitleElement = document.createElement('h2');
+    const bookTitleElement = window.document.createElement('h2');
     bookTitleElement.textContent = book.name;
     mainElement.appendChild(bookTitleElement);
   });
